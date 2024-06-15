89cc5a07ed5830d9b347bad895fb153e9676f894
Futte <xFutte@gmail.com>
2024-06-15 16:31:59 +0000

    Added obfuscator

--- svelte-source/vite.config.ts
+++ svelte-source/vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import * as path from 'path';
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,13 @@ export default defineConfig({
     targets: [
       { src: 'src/config.json', dest: '' } // Adjust the destination folder as needed
     ],
-  })],
+  }), obfuscatorPlugin({
+    options: {
+      // your javascript-obfuscator options
+      debugProtection: true,
+      // ...  [See more options](https://github.com/javascript-obfuscator/javascript-obfuscator)
+    },
+  }) ],
   build: {
     outDir: path.resolve(__dirname, '../html'), // Output to root/html
     emptyOutDir: true, // Ensure the directory is emptied before building
